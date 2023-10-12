da3b4c6ab8217af121c6168686e90dce580144ac
cmj <chmin82@gmail.com>
2023-10-12 22:29:19 +0900

    [CHORE] update
    - appspec.yml
    - gradle-publish.yml
    - scripts.sh

---
+++ scripts/stop.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# 엔진엑스와 연결되어 있지 않은 스프링 부트를 종료하는 스크립트
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
