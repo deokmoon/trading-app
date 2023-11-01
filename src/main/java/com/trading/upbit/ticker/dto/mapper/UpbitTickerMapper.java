0eb4b78086efe393337f75c0485826360b6e3cfa
deokmoon <deok2moon@gmail.com>
2023-11-01 23:42:28 +0900

    [CHORE] MapStruct 의존성 주입 및 적용

---
+++ src/main/java/com/trading/upbit/ticker/dto/mapper/UpbitTickerMapper.java
@@ -0,0 +1,10 @@
+package com.trading.upbit.ticker.dto.mapper;
+
+import com.trading.upbit.ticker.domain.UpbitTicker;
+import com.trading.upbit.ticker.dto.UpbitTickerResponseDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UpbitTickerMapper {
+    UpbitTicker toTicker(UpbitTickerResponseDto tickerResponseDto);
+}
