3e804756b58c3d625dd234c0f4dc15d3625d612e
deokmoon <deok2moon@gmail.com>
2023-11-01 23:46:01 +0900

    [REFACTOR] 마켓코드 정보 조회 후 Socket 연결하도록 빈 의존성 적용

--- src/main/java/com/trading/upbit/market/schedular/MarketScheduler.java
+++ src/main/java/com/trading/upbit/market/schedular/MarketScheduler.java
@@ -1,6 +1,6 @@
 package com.trading.upbit.market.schedular;
 
-import com.trading.upbit.market.application.MarketPriceService;
+import com.trading.upbit.market.application.MarketBaseService;
 import com.trading.upbit.market.dto.InquiryAllMarketInformationDto;
 import com.trading.upbit.market.feign.MarketCodeInquiry;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +13,15 @@ import static com.trading.util.ConvertStringToDto.convertListDtoFromJson;
 
 @Component
 @RequiredArgsConstructor
-public class MarketPriceScheduler {
-
-    private final MarketPriceService marketPriceService;
+public class MarketScheduler {
+    private final MarketBaseService marketBaseService;
     private final MarketCodeInquiry marketCodeInquiry;
 
     @Scheduled(fixedDelayString = "${schedule.fixed.delay.upbit}")
     public void retrieveAndSaveTicker() {
         // 전체 종목 조회
         List<InquiryAllMarketInformationDto> marketInfoDtoList = convertListDtoFromJson(marketCodeInquiry.getAllMarketInformation().getBody(), InquiryAllMarketInformationDto.class);
-        marketPriceService.createMarketInfoJsonFile(marketInfoDtoList);
+        marketBaseService.createMarketInfoJsonFile(marketInfoDtoList);
+        marketBaseService.saveMarketBase(marketInfoDtoList);
     }
 }
