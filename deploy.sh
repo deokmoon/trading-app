39d9ff00bf86dd9bce0ca7a663fd112bf232edde
cmj <chmin82@gmail.com>
2023-11-15 22:23:24 +0900

    [TRAD-26] deploy.sh 생성

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 직접 배포 시 사용되는 스크립트
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
+
+echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr build/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup /opt/java/openjdk/bin/java -jar $JAR_NAME > nohup.out 2>&1 &
+
+echo "> 배포완료"
