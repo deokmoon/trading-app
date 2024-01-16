1f59f537805195f8801fd1b78dd61ff9a8e0a515
deokmoon <deok2moon@gmail.com>
2024-01-17 01:22:58 +0900

    [FEAT][TRAD-8] 게시판 댓글 기능 적용

--- src/main/java/com/trading/board/domain/FeedComment.java
+++ src/main/java/com/trading/board/domain/FeedComment.java
@@ -1,5 +1,6 @@
 package com.trading.board.domain;
 
+import com.trading.board.dto.FeedBoardCommentRequest;
 import com.trading.config.orm.BaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
@@ -11,14 +12,20 @@ import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class FeedComment extends BaseTimeEntity {
 
     @Id
@@ -33,10 +40,19 @@ public class FeedComment extends BaseTimeEntity {
     private byte[] image;
 
     @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
-    private List<FeedCommentReply> replies;
+    private List<FeedCommentReply> replies = new ArrayList<>();
 
     private String comment;
     private String writer;
     private int likeCount;
 
+    public static FeedComment from(FeedBoardCommentRequest request, FeedBoard board) throws IOException {
+        return FeedComment.builder()
+                .image(request.getImage() == null ? new byte[]{} : request.getImage().getBytes())
+                .board(board)
+                .comment(request.getComment())
+                .writer(request.getWriter())
+                .likeCount(0)
+                .build();
+    }
 }
