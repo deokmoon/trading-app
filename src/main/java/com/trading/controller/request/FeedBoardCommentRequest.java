a12aa09c486163b298f477e2243189b791443b64
deokmoon <deok2moon@gmail.com>
2024-02-03 12:56:40 +0900

    [FIX][TRAD-8] git conflict 처리

---
+++ src/main/java/com/trading/controller/request/FeedBoardCommentRequest.java
@@ -0,0 +1,17 @@
+package com.trading.controller.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class FeedBoardCommentRequest {
+
+    private MultipartFile image;;
+    private String writer;
+    private String comment;
+    private String boardId;
+
+}
